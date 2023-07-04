43f9ed7e6c931fd794439dcfbed0c83a35dc337e
Barış Neşeli <114444125+neselibaris@users.noreply.github.com>
2023-07-05 01:30:13 +0300

    Add files via upload

---
+++ ödev/ekstra.py
@@ -0,0 +1,33 @@
+ad = input("Adınızı girin: ")
+soyad = input("Soyadınızı girin: ")
+dogum_tarihi = input("Doğum tarihinizi girin: ")
+
+print("Ad:", ad)
+print("Soyad:", soyad)
+print("Doğum Tarihi:", dogum_tarihi)
+
+
+
+
+
+
+not1 = float(input("1. sınav notunu girin: "))
+not2 = float(input("2. sınav notunu girin: "))
+not3 = float(input("3. sınav notunu girin: "))
+not4 = float(input("4. sınav notunu girin: "))
+
+ortalama = (not1 + not2 + not3 + not4) / 4
+
+print("Not Ortalaması:", ortalama)
+
+
+
+
+
+
+cumle = input("Bir cümle girin: ")
+
+baslik_cumlesi = cumle.title()
+
+print("Başlık Stili:")
+print(baslik_cumlesi)
